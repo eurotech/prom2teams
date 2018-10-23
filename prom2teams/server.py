ce154014f3aeafff45889d37afcd71488a668616
Stefano Zilli <stefano.zilli@eurotech.com>
2018-10-23 13:38:04 +0200

    Change server configuration

--- prom2teams/server.py
+++ prom2teams/server.py
@@ -1,7 +1,6 @@
 import logging
 
 import http.server
-import socketserver
 
 LOGGER = logging.getLogger(__name__)
 
@@ -10,15 +9,22 @@ class Server(object):
     class ServerHandler(http.server.SimpleHTTPRequestHandler):
 
         def do_POST(self):
-            content_len = int(self.headers.get('content-length', 0))
-            post_body = self.rfile.read(content_len)
-            self.post_callback(post_body)
+            # Send the response
+            self.send_response(200)
+            self.end_headers()
+            # Process the content
+            try:
+                content_len = int(self.headers.get('content-length', 0))
+                self.post_callback(self.rfile.read(content_len))
+            except Exception as e:
+                LOGGER.error("Failed to process request: %s", e)
 
     def listen(self, host, port, processor):
         handler = self.ServerHandler
         handler.post_callback = processor
 
-        httpd = socketserver.TCPServer((host, port), handler)
+        httpd = http.server.HTTPServer((host, port), handler)
+        httpd.protocol_version = 'HTTP/1.1'
 
         LOGGER.info("Listening on %s:%s", host, port)
         httpd.serve_forever()
